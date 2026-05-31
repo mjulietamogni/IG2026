9a6693c735ad1127105f6f97e79ab59b24279c1f
Julieta Mogni <majumogni@gmail.com>
2026-05-31 12:16:13 -0300

    commit de practicas

---
+++ practica-16/js/script.js
@@ -0,0 +1,16 @@
+let nombre = 'Julieta'; 
+let apellido = "Mogni";
+let alumna = nombre + " " + apellido;
+let edad ="26 años"; 
+
+const universidad ="Universidad Nacional de las Artes"; 
+
+/*cambio de variables y constante*/
+
+nombre = "Maria"; 
+apellido = "Varela"; 
+edad = "27 años"
+universidad = "UBA"; 
+
+
+/*Al modificar el valor de una variable, cambia su valor al llamarla, en cambio la constante no cambia su valor, este se define una unica vez. */
